28559c09c4307e7899776d32563cc222bfd4488d
Serge Krot <serge.krot@cib.de>
2025-03-31 19:46:11 +0200

    correct info

--- frontend/src/components/modals/AboutModal.vue
+++ frontend/src/components/modals/AboutModal.vue
@@ -41,7 +41,7 @@ export default {
     })
     .catch(error => {
       console.error("Error loading version:", error)
-      this.version = "Unable to load"
+      this.version = "n/a"
     })
   },
   methods: {
