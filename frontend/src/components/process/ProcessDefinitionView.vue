ff02515822e34906cb84959dd5241cd4e3e2d9a1
Serge Krot <serge.krot@cib.de>
2025-04-01 19:20:54 +0200

    fixed: 'nextTick' is defined but never used  no-unused-vars

--- frontend/src/components/process/ProcessDefinitionView.vue
+++ frontend/src/components/process/ProcessDefinitionView.vue
@@ -51,9 +51,8 @@
 </template>
 
 <script>
-import { nextTick } from 'vue'
 import moment from 'moment'
-import { TaskService, ProcessService, HistoryService, 
+import { TaskService, ProcessService, HistoryService,
   IncidentService, JobService, JobDefinitionService } from '@/services.js'
 import ProcessInstancesView from '@/components/process/ProcessInstancesView.vue'
 import ProcessDetailsSidebar from '@/components/process/ProcessDetailsSidebar.vue'
