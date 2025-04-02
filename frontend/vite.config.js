0301c27ec113bf32d9f4ecacdc34797e9cc7956b
fatima <fatima@cib.de>
2025-04-02 07:55:14 +0200

    feature(vite.config.js): add proxy for bootstrap css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-const backendUrl = 'http://localhost:8080'
+const backendUrl = 'http://localhost:8090'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -24,6 +24,22 @@ export default defineConfig({
   server: {
     proxy: {
 		  '/webapp/services': {
+        target: backendUrl,
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err)
+          })
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            // console.log('Sending Request to the Target:', req.method, backendUrl + req.url)
+          })
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            // console.log('Received Response from the Target:', proxyRes.statusCode, backendUrl + req.url)
+          })
+        },
+      },
+      '/webapp/bootstrap': {
         target: backendUrl,
         changeOrigin: true,
         secure: false,
