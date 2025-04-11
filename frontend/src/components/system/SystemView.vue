e671e3242dd0f3562b1c4a8ea50be0dd05defca0
Serge Krot <serge.krot@cib.de>
2025-04-11 09:28:37 +0200

    enhance: highlight active item in the list

--- frontend/src/components/system/SystemView.vue
+++ frontend/src/components/system/SystemView.vue
@@ -3,13 +3,14 @@
     <SidebarsFlow ref="sidebars" class="border-top overflow-auto" v-model:left-open="leftOpen" :left-caption="leftCaption">
       <template v-slot:left>
         <b-list-group>
-          <b-list-group-item 
-            v-for="setting in systemSettings" 
-            :key="setting" 
-            class="border-0 px-3 py-2"
-            action 
+          <b-list-group-item
+            v-for="setting in systemSettings"
+            :key="setting"
+            class="border-0 px-3 py-2 no-radius-right"
+            :active="$route.path.includes(`/seven/auth/admin/system/${setting}`)"
+            action
             :to="`/seven/auth/admin/system/${setting}`">
-            <span> {{ $t(`admin.system.${setting}.title`) }}</span>
+            <span>{{ $t(`admin.system.${setting}.title`) }}</span>
           </b-list-group-item>
         </b-list-group>
       </template>
@@ -19,7 +20,6 @@
 </template>
 
 <script>
-
 import SidebarsFlow from '@/components/common-components/SidebarsFlow.vue'
 
 export default {
@@ -40,3 +40,10 @@ export default {
   }
 }
 </script>
+
+<style lang="css" scoped>
+.no-radius-right {
+  border-top-right-radius: 0 !important;
+  border-bottom-right-radius: 0 !important;
+}
+</style>
