d26f70731990e7e988d074adca1ea2091c6a4d77
Serge Krot <serge.krot@cib.de>
2025-04-14 10:21:38 +0200

    enhance(ui) human tasks: unify top gap

--- frontend/src/components/task/HumanTasksView.vue
+++ frontend/src/components/task/HumanTasksView.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container-fluid bg-light pt-3 overflow-auto">
     <div class="row justify-content-around">
-      <div class="bg-white col-md-5 col-12 shadow p-3 mb-3">
+      <div class="bg-white col-md-5 col-12 shadow p-3 my-3">
         <h3>{{ $t('human-tasks.assignmentsByType') }}</h3>
         <hr>
-        <FlowTable striped resizable thead-class="sticky-header" :items="countsByType" 
+        <FlowTable striped resizable thead-class="sticky-header" :items="countsByType"
           primary-key="id" prefix="human-tasks."
           sort-by="label" :fields="[
-          { label: 'tasks', key: 'tasks', class: 'col-3', tdClass: 'py-1 border-top-0 fw-bold justify-content-center', 
+          { label: 'tasks', key: 'tasks', class: 'col-3', tdClass: 'py-1 border-top-0 fw-bold justify-content-center',
             thClass: 'd-flex justify-content-center', sortable: false },
           { label: 'types', key: 'types', class: 'col-9', tdClass: 'py-1 border-top-0', sortable: false }]">
           <template v-slot:cell(types)="table">
@@ -15,13 +15,13 @@
           </template>
         </FlowTable>
       </div>
-      <div class="bg-white col-md-5 col-12 shadow p-3 mb-3">
+      <div class="bg-white col-md-5 col-12 shadow p-3 my-3">
         <h3>{{ $t('human-tasks.assignmentsByGroup') }} <span v-b-popover.hover.right="$t('human-tasks.assignmentsByGroupInfo')" class="mdi mdi-18px mdi-information-outline text-info"></span></h3>
         <hr>
-        <FlowTable striped resizable thead-class="sticky-header" :items="taskCountByCandidateGroup" 
+        <FlowTable striped resizable thead-class="sticky-header" :items="taskCountByCandidateGroup"
           primary-key="id" prefix="human-tasks."
           sort-by="label" :fields="[
-          { label: 'tasks', key: 'taskCount', class: 'col-3', tdClass: 'py-1 border-top-0 justify-content-center', 
+          { label: 'tasks', key: 'taskCount', class: 'col-3', tdClass: 'py-1 border-top-0 justify-content-center',
             thClass: 'd-flex justify-content-center', sortable: false },
           { label: 'groupName', key: 'groupName', class: 'col-9', tdClass: 'py-1 border-top-0', sortable: false }]">
           <template v-slot:cell(groupName)="table">
@@ -34,7 +34,6 @@
 </template>
 
 <script>
-
 import { TaskService } from '@/services.js'
 import FlowTable from '@/components/common-components/FlowTable.vue'
 
@@ -56,11 +55,11 @@ export default {
     TaskService.findHistoryTaksCount({ unfinished: true, assigned: true }).then(data => {
       this.countsByType[0].tasks = data
     })
-    TaskService.findHistoryTaksCount({ unfinished: true, 
+    TaskService.findHistoryTaksCount({ unfinished: true,
       unassigned: true, withCandidateGroups: true }).then(data => {
       this.countsByType[1].tasks = data
     })
-    TaskService.findHistoryTaksCount({ unfinished: true, 
+    TaskService.findHistoryTaksCount({ unfinished: true,
       unassigned: true, withoutCandidateGroups: true }).then(data => {
       this.countsByType[2].tasks = data
     })
