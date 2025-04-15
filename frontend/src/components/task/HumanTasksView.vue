7afbac2b1dbe41736a7e069a1d938b6ac8a95fb3
Serge Krot <serge.krot@cib.de>
2025-04-15 10:57:50 +0200

    human tasks: use one design like in dashboard

--- frontend/src/components/task/HumanTasksView.vue
+++ frontend/src/components/task/HumanTasksView.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid bg-light pt-3 overflow-auto">
     <div class="row justify-content-around">
       <div class="col-12 col-md-5 p-3 my-3 bg-white border rounded shadow-sm">
-        <h3>{{ $t('human-tasks.assignmentsByType') }}</h3>
+        <h5>{{ $t('human-tasks.assignmentsByType') }}</h5>
         <hr>
         <FlowTable striped resizable thead-class="sticky-header" :items="countsByType"
           primary-key="id" prefix="human-tasks."
@@ -16,7 +16,7 @@
         </FlowTable>
       </div>
       <div class="col-12 col-md-5 p-3 my-3 bg-white border rounded shadow-sm">
-        <h3>{{ $t('human-tasks.assignmentsByGroup') }} <span v-b-popover.hover.right="$t('human-tasks.assignmentsByGroupInfo')" class="mdi mdi-18px mdi-information-outline text-info"></span></h3>
+        <h5>{{ $t('human-tasks.assignmentsByGroup') }} <span v-b-popover.hover.right="$t('human-tasks.assignmentsByGroupInfo')" class="mdi mdi-18px mdi-information-outline text-info"></span></h5>
         <hr>
         <FlowTable striped resizable thead-class="sticky-header" :items="taskCountByCandidateGroup"
           primary-key="id" prefix="human-tasks."
