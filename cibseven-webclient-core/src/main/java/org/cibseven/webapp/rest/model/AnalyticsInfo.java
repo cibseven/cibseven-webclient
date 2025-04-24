eef04a4646f1c2bd001e34e8e7439d80a2431bf3
adrian <Adrian.Alejo@cib.de>
2025-04-24 18:44:02 +0100

    fix: Implement fixes and refactor code for the dashboard processes analytics

--- cibseven-webclient-core/src/main/java/org/cibseven/webapp/rest/model/AnalyticsInfo.java
+++ cibseven-webclient-core/src/main/java/org/cibseven/webapp/rest/model/AnalyticsInfo.java
@@ -2,9 +2,11 @@ package org.cibseven.webapp.rest.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data @JsonIgnoreProperties(ignoreUnknown = true) 
+@Data @AllArgsConstructor @NoArgsConstructor @JsonIgnoreProperties(ignoreUnknown = true) 
 public class AnalyticsInfo {
   private String id;
   private String title;
