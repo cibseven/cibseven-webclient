b9496e26f210a52cd936f2b099b24b1fefb55b3b
ivanfarray-cib <ivan.hernandezfarray@cib.de>
2025-04-24 14:23:53 +0100

    fix(vite.config): comment logs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -60,10 +60,10 @@ export default defineConfig({
             console.log('proxy error', err)
           })
           proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, backendUrl + req.url)
+            //console.log('Sending Request to the Target:', req.method, backendUrl + req.url)
           })
           proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, backendUrl + req.url)
+            //console.log('Received Response from the Target:', proxyRes.statusCode, backendUrl + req.url)
           })
         },
       },      
