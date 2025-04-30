717c9fdc7c9eb77d7d41c6bf3bb51df539aecae4
Dmitry Malkovich <dmitry.malkovich@cib.de>
2025-04-30 19:27:54 +0300

    fix(start process): favorites do not appear after refreshing the webpage (#163)
    * fix(favorites): favorities do not appear after refreshing the webpage
    
    CIB7-300
    
    * fix(favorites): add comments
    
    CIB7-300

--- frontend/src/components/start-process/StartProcessList.vue
+++ frontend/src/components/start-process/StartProcessList.vue
@@ -118,7 +118,7 @@ export default {
     }
   },
   created: function() {    
-    this.loadProcesses(false)
+    this.loadProcesses(false) // the method takes localStorage.getItem('favorites') into account
     this.view = this.isMobile() ? 'image-outline' : localStorage.getItem('viewMode') || 'image-outline'
   },
   computed: {
@@ -148,9 +148,6 @@ export default {
     }
   },
   mounted: function() {
-    if (localStorage.getItem('favorites')) {
-      this.$store.dispatch('setFavorites', { favorites: JSON.parse(localStorage.getItem('favorites')) })
-    }
     if (this.$route.query.key) {
       setTimeout(() => { this.checkProcessInUrl(this.$route.query.key) }, 1000)
     }
