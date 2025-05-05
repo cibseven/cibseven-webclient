b2724171cd8776dadab1752a465cd725df4f8479
cibdmitrym <dmitry.malkovich@cib.de>
2025-05-05 13:57:32 +0300

    chore(wildfly): remove SpringApplicationBuilder.configure

--- cibseven-webclient-web/src/main/java/org/cibseven/webapp/Application.java
+++ cibseven-webclient-web/src/main/java/org/cibseven/webapp/Application.java
@@ -18,7 +18,6 @@ package org.cibseven.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -27,9 +26,4 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
 }
