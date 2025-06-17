d803afc38bc0db901265d39a1843d4366052ddc5
Patrick <patrick.fincke@cib.de>
2025-06-17 16:08:12 +0200

    fix(bpm-sdk): Adjust build to standard

--- bpm-sdk/vite.config.js
+++ bpm-sdk/vite.config.js
@@ -1,21 +1,29 @@
 import { defineConfig } from 'vite';
 import path from 'node:path';
 
+// Detect build mode
+const isLibrary = process.env.BUILD_MODE === 'library';
+
+console.log('isLibrary', isLibrary);
+
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'index.js'),
-      name: 'BpmSdk',
-      fileName: (format) => `bpm-sdk.${format}.js`,
-    },
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      // Externalize dependencies if needed
-      external: [],
-      output: {
-        globals: {},
-      },
-    },
-  },
+  build: isLibrary
+    ? {
+        lib: {
+          entry: path.resolve(__dirname, 'index.js'),
+          name: 'BpmSdk',
+          formats: ['es', 'umd'],
+          fileName: (format) => `bpm-sdk.${format}.js`,
+        },
+        outDir: 'dist',
+        emptyOutDir: true,
+        rollupOptions: {
+          external: [], // Add external deps here if needed
+          output: {
+            globals: {}, // Add globals here if needed
+            assetFileNames: 'bpm-sdk.[ext]',
+          },
+        },
+      }
+    : {},
 });
