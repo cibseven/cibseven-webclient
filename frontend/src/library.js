6122e6ab9f08ef9143b2f1a632c8cf5fb2452bf2
adrian90 <adrian90.at@gmail.com>
2025-07-31 12:43:53 +0100

    chore(library): export BatchService (#380)
    Co-authored-by: Adrian Alejo Trujillo <Adrian@cib.de>

--- frontend/src/library.js
+++ frontend/src/library.js
@@ -134,7 +134,8 @@ import SystemDiagnostics from '@/components/system/SystemDiagnostics.vue'
 import ExecutionMetrics from '@/components/system/ExecutionMetrics.vue'
 import ShortcutsModal from '@/components/modals/ShortcutsModal.vue'
 import ShortcutsTable from '@/components/modals/ShortcutsTable.vue'
-import { TaskService, HistoryService, ProcessService, getServicesBasePath, setServicesBasePath, IncidentService, DecisionService } from '@/services.js';
+import { TaskService, HistoryService, ProcessService, getServicesBasePath, 
+  setServicesBasePath, IncidentService, DecisionService, BatchService } from '@/services.js';
 import DeployedForm from '@/components/forms/DeployedForm.vue'
 import StartDeployedForm from '@/components/forms/StartDeployedForm.vue'
 import DecisionDefinitionDetails from '@/components/decision/DecisionDefinitionDetails.vue'
@@ -343,6 +344,7 @@ export {
   TaskService,
   HistoryService,
   ProcessService,
+  BatchService,
   DecisionService,
   getServicesBasePath,
   setServicesBasePath,
