b7276168cbc775d2b8635150ba5f41d9e417f22d
Adrian Alejo Trujillo <Adrian@cib.de>
2025-09-11 17:18:41 +0100

    feat(decisions): update decisions style

--- frontend/src/components/decisions/list/DecisionList.vue
+++ frontend/src/components/decisions/list/DecisionList.vue
@@ -17,30 +17,44 @@
 
 -->
 <template>
-  <div class="d-flex flex-column bg-light" :style="{ height: 'calc(100% - 55px)' }">
-    <div class="container pt-4">
-      <div class="row align-items-center pb-2">
-        <div class="col-4">
-          <b-input-group size="sm">
-            <template #prepend>
-              <b-button :title="$t('searches.search')" aria-hidden="true" class="rounded-left" variant="secondary"><span class="mdi mdi-magnify" style="line-height: initial"></span></b-button>
-            </template>
-            <b-form-input :title="$t('searches.search')" :placeholder="$t('searches.search')" v-model.trim="filter"></b-form-input>
-          </b-input-group>
+  <div class="d-flex flex-column bg-light pt-3">
+    <div class="d-flex flex-column container bg-white border rounded shadow-sm p-0 mt-3 mb-3 overflow-y-auto">
+      <div class="container pt-4">
+        <div class="row align-items-center pb-1">
+          <div class="col-8">
+            <div class="border rounded d-flex flex-fill align-items-center">
+              <b-button @click.stop="null"
+                size="sm" class="mdi mdi-magnify mdi-24px text-secondary" variant="link"
+                :title="$t('searches.refreshAndFilter')"></b-button>
+              <div class="flex-grow-1">
+                <input
+                  type="text"
+                  v-model.trim="filter"
+                  :placeholder="$t('searches.filter')"
+                  class="form-control-plaintext w-100"
+                />
+              </div>
+              <div class="block text-secondary ms-2 me-3 text-nowrap"
+                :title="$t('start.cockpit.decisions.title') + ': ' + decisionsFiltered.length"
+              >{{ decisionsFiltered.length }}</div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="container overflow-auto h-100 bg-white shadow-sm border rounded g-0">
-      <FlowTable :items="decisionsFiltered" thead-class="sticky-header" striped native-layout primary-key="id" prefix="decision." :fields="fields" @click="goToDecision($event)" @select="focused = $event[0]" @mouseenter="focused = $event" @mouseleave="focused = null">
-        <template v-slot:cell(actions)="table">
-          <b-button :disabled="focused !== table.item" style="opacity: 1" @click.stop="goToDecision(table.item)" class="px-2 border-0 shadow-none" :title="$t('decision.showManagement')" variant="link">
-            <span class="mdi mdi-18px mdi-account-tie-outline"></span>
-          </b-button>
-        </template>
-      </FlowTable>
-      <div v-if="!decisionsFiltered.length">
-        <img :alt="$t(textEmptyDecisionsList)" src="@/assets/images/decision/empty_processes_list.svg" class="d-block mx-auto mt-5 mb-3" style="max-width: 250px">
-        <div class="h5 text-secondary text-center">{{ $t(textEmptyDecisionsList) }}</div>
+      <div class="container overflow-auto h-100 rounded g-0">
+        <div class="m-3 mb-0">
+          <FlowTable :items="decisionsFiltered" thead-class="sticky-header" striped native-layout primary-key="id" prefix="decision." :fields="fields" @click="goToDecision($event)" @select="focused = $event[0]" @mouseenter="focused = $event" @mouseleave="focused = null">
+            <template v-slot:cell(actions)="table">
+              <b-button :disabled="focused !== table.item" style="opacity: 1" @click.stop="goToDecision(table.item)" class="px-2 border-0 shadow-none" :title="$t('decision.showManagement')" variant="link">
+                <span class="mdi mdi-18px mdi-account-tie-outline"></span>
+              </b-button>
+            </template>
+          </FlowTable>
+          <div v-if="!decisionsFiltered.length">
+            <img :alt="$t(textEmptyDecisionsList)" src="@/assets/images/decision/empty_processes_list.svg" class="d-block mx-auto mt-5 mb-3" style="max-width: 250px">
+            <div class="h5 text-secondary text-center">{{ $t(textEmptyDecisionsList) }}</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
