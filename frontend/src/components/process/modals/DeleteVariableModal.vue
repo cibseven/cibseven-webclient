2264640db84ef73ab4932c489f7e6acaf637e076
Serge Krot <serge.krot@cib.de>
2025-09-16 12:51:30 +0200

    fix(variables) delete runtime/historic variable (#497)

--- frontend/src/components/process/modals/DeleteVariableModal.vue
+++ frontend/src/components/process/modals/DeleteVariableModal.vue
@@ -17,36 +17,102 @@
 
 -->
 <template>
-  <ConfirmDialog v-if="params" ref="confirm"
-    @ok="params.ok(params.variable)"
-    :ok-title="$t('confirm.delete')">
+  <b-modal ref='confirm' v-if="variable" :title="$t('confirm.title')">
+    <div class="row">
+      <div class="col-2 d-flex justify-content-center">
+        <span class="mdi-36px mdi mdi-alert-outline text-warning"></span>
+      </div>
+      <div class="col-10 d-flex align-items-center ps-0">
+        <div>
+          <p>{{ $t('process-instance.variables.confirmDelete') }}</p>
 
-    <p>{{ $t('process-instance.variables.confirmDelete') }}</p>
+          <p>
+            {{ $t('process-instance.variables.name') }}: <strong>{{ variable?.name }}</strong>
+            <br>
+            {{ $t('process-instance.variables.type') }}: <strong>{{ variable?.type }}</strong>
+          </p>
 
-    <p>
-      {{ $t('process-instance.variables.name') }}: <strong>{{ params?.variable?.name }}</strong>
-      <br>
-      {{ $t('process-instance.variables.type') }}: <strong>{{ params?.variable?.type }}</strong>
-    </p>
-
-  </ConfirmDialog>
+          <div v-if="errorMessage" class="alert alert-danger text-danger d-flex align-items-center">
+            <div class="me-4">
+              <span class="mdi-36px mdi mdi-alert-octagon-outline text-danger"></span>
+            </div>
+            <div>
+              <p class="ms-0">{{ errorTitle }}</p>
+              <p class="mb-0">{{ errorMessage }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <template v-slot:modal-footer>
+      <div class="row w-100 me-0">
+        <div class="col-4 p-0">
+          <BWaitingBox v-if="deleting" class="d-inline me-2" styling="width: 25px"></BWaitingBox>
+        </div>
+        <div class="col-8 p-0 text-end">
+          <b-button :disabled="deleting" @click="$refs.confirm.hide('cancel')" variant="link">{{ $t('confirm.cancel') }}</b-button>
+          <b-button :disabled="deleting" @click="onDelete" variant="primary">{{ $t('confirm.delete') }}</b-button>
+        </div>
+      </div>
+    </template>
+  </b-modal>
 </template>
 
 <script>
-import ConfirmDialog from '@/components/common-components/ConfirmDialog.vue'
+import { BWaitingBox } from 'cib-common-components'
+import { ProcessService, HistoryService } from '@/services.js'
 
 export default {
   name: 'DeleteVariableModal',
-  components: { ConfirmDialog },
+  emits: ['variable-deleted'],
+  components: { BWaitingBox },
   data: function() {
     return {
-      params: Object
+      isInstanceActive: false,
+      variable: Object,
+      deleting: false,
+      errorMessage: '',
+    }
+  },
+  computed: {
+    errorTitle() {
+      if (!this.errorMessage || !this.variable) {
+        return ''
+      }
+      const messageTemplate = this.isInstanceActive ? 'process-instance.variables.deleteStatus.runtimeError' : 'process-instance.variables.deleteStatus.historicError'
+      return this.$t(messageTemplate, { name: this.variable.name })
     }
   },
   methods: {
-    show: function(params) {
-      this.params = params
+    show: function(isInstanceActive, variable) {
+      this.isInstanceActive = isInstanceActive
+      this.variable = variable
+      this.errorMessage = ''
+      this.deleting = false
+
       this.$refs.confirm.show()
+    },
+    async onDelete() {
+      this.deleting = true
+      this.errorMessage = ''
+
+      try {
+        if (this.isInstanceActive) {
+          await ProcessService.deleteVariableByExecutionId(this.variable.executionId, this.variable.name)
+        } else {
+          await HistoryService.deleteVariableHistoryInstance(this.variable.id)
+        }
+      } catch (error) {
+        this.errorMessage = error.message
+      }
+
+      if (!this.errorMessage) {
+        this.$refs.confirm.hide('ok')
+        this.$emit('variable-deleted', this.variable)
+      }
+      else {
+        this.deleting = false
+      }
     }
   }
 }
