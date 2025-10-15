c2392866c3130d09c1a95c90ff0f194fc60242ca
Fernando Mambrilla Garcia <fernandom@cib.de>
2025-10-15 12:49:58 +0200

    clean(oauth2): tokenCache using lombok

--- cibseven-webclient-core/src/main/java/org/cibseven/webapp/auth/sso/TokenCache.java
+++ cibseven-webclient-core/src/main/java/org/cibseven/webapp/auth/sso/TokenCache.java
@@ -2,25 +2,12 @@ package org.cibseven.webapp.auth.sso;
 
 import java.util.Date;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class TokenCache {
 	private final String accessToken;
 	private final Date expiration;
-	private final Object lock = new Object(); 
-
-	public TokenCache(String accessToken, Date expiration) {
-		this.accessToken = accessToken;
-		this.expiration = expiration;
-	}
-
-	public String getAccessToken() {
-		return accessToken;
-	}
-
-	public Date getExpiration() {
-		return expiration;
-	}
-
-	public Object getLock() {
-		return lock;
-	}
 }
