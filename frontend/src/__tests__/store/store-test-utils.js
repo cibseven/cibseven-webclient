910370018090c33ba1adc698fc9e48a13d2cd428
Serge Krot <serge.krot@cib.de>
2026-01-14 12:23:02 +0100

    chore(tests) prefer using an optional chain expression instead, as it's more concise and easier to read

--- frontend/src/__tests__/store/store-test-utils.js
+++ frontend/src/__tests__/store/store-test-utils.js
@@ -80,9 +80,8 @@ export const createStoreTestSuite = (storeName, storeModule, customTests = {}) =
           })
 
           // Custom mutation tests
-          if (customTests.mutations && customTests.mutations[mutationName]) {
-            customTests.mutations[mutationName](mutation, () => state, mutationName)
-          }
+          const mutationTest = customTests.mutations?.[mutationName]
+          mutationTest?.(mutation, () => state, mutationName)
         })
       })
     }
@@ -104,9 +103,8 @@ export const createStoreTestSuite = (storeName, storeModule, customTests = {}) =
           })
 
           // Custom getter tests
-          if (customTests.getters && customTests.getters[getterName]) {
-            customTests.getters[getterName](getter, () => state, getterName)
-          }
+          const getterTest = customTests.getters?.[getterName]
+          getterTest?.(getter, () => state, getterName)
         })
       })
     }
@@ -122,10 +120,9 @@ export const createStoreTestSuite = (storeName, storeModule, customTests = {}) =
           })
 
           // Custom action tests
-          if (customTests.actions && customTests.actions[actionName]) {
-            const getContext = () => ({ commit: mockCommit, dispatch: mockDispatch, state, getters: mockGetters })
-            customTests.actions[actionName](action, getContext, actionName)
-          }
+          const getContext = () => ({ commit: mockCommit, dispatch: mockDispatch, state, getters: mockGetters })
+          const actionTest = customTests.actions?.[actionName]
+          actionTest?.(action, getContext, actionName)          
         })
       })
     }
