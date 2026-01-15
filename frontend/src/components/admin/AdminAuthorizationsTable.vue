4ac5d383dff4d23cdfbe808baa4b443e4525e028
Serge Krot <serge.krot@cib.de>
2026-01-15 14:59:01 +0100

    chore(admin) refactor action buttons in rows in admin components

--- frontend/src/components/admin/AdminAuthorizationsTable.vue
+++ frontend/src/components/admin/AdminAuthorizationsTable.vue
@@ -45,7 +45,7 @@
         <FlowTable striped thead-class="sticky-header light" :items="authorizations" primary-key="id"
           prefix="admin.authorizations." :fields="authorizationFields"
           class="shadow-sm border rounded"
-          @contextmenu="focused = $event" @mouseenter="focused = $event" @mouseleave="focused = null">
+        >
           <template v-slot:cell(type)="row">
             <div class="w-100 d-flex align-items-center" v-if="edit === row.item.id && edit === '0'">
               <span> {{ $t('admin.authorizations.types.' + types[row.item.type].key) }} </span>
@@ -132,19 +132,14 @@
             <div v-else> {{ row.item.resourceId }} </div>
           </template>
           <template v-slot:cell(actions)="row">
-            <div class="d-flex">
-              <div v-if="edit === row.item.id">
-                <b-button style="opacity: 1" class="px-2 border-0 shadow-none" variant="link" @click="save(row.item)"
-                  :disabled="focused !== row.item || (row.item.userId == null && row.item.groupId == null)"><span class="mdi mdi-18px mdi-content-save-outline"></span></b-button>
-                <span class="border-start h-50" :class="focused === row.item ? 'border-secondary' : ''"></span>
-                <b-button :disabled="focused !== row.item" style="opacity: 1" class="px-2 border-0 shadow-none" variant="link" @click="cancelEdit(row.item)"><span class="mdi mdi-18px mdi-block-helper"></span></b-button>
-              </div>
-              <div v-else>
-                <b-button :disabled="focused !== row.item" style="opacity: 1" variant="link" class="px-2 border-0 shadow-none" @click="prepareEdit(row.item)"><span class="mdi mdi-18px mdi-pencil-outline"></span></b-button>
-                <span class="border-start h-50" :class="focused === row.item ? 'border-secondary' : ''"></span>
-                <b-button :disabled="focused !== row.item" style="opacity: 1" variant="link" class="px-2 border-0 shadow-none" @click="prepareRemove(row.item)"><span class="mdi mdi-18px mdi-delete-outline"></span></b-button>
-              </div>
-            </div>
+            <template v-if="edit === row.item.id">
+              <CellActionButton @click="save(row.item)" :disabled="row.item.userId == null && row.item.groupId == null" :title="$t('admin.authorizations.buttons.save')" icon="mdi-content-save-outline"></CellActionButton>
+              <CellActionButton @click="cancelEdit(row.item)" :title="$t('admin.authorizations.buttons.cancel')" icon="mdi-block-helper"></CellActionButton>
+            </template>
+            <template v-else>
+              <CellActionButton @click="prepareEdit(row.item)" :title="$t('admin.authorizations.buttons.edit')" icon="mdi-pencil-outline"></CellActionButton>
+              <CellActionButton @click="prepareRemove(row.item)" :title="$t('admin.authorizations.buttons.delete')" icon="mdi-delete-outline"></CellActionButton>
+            </template>
           </template>
         </FlowTable>
         <div class="text-center w-100" v-if="loading">
@@ -180,14 +175,14 @@ import { moment } from '@/globals.js'
 import { debounce } from '@/utils/debounce.js'
 import { getStringObjByKeys } from '@/components/admin/utils.js'
 import { FlowTable, TaskPopper, ConfirmDialog, BWaitingBox } from '@cib/common-frontend'
+import CellActionButton from '@/components/common-components/CellActionButton.vue'
 
 export default {
   name: 'AdminAuthorizationsTable',
-  components: { FlowTable, TaskPopper, BWaitingBox, ConfirmDialog },
+  components: { FlowTable, TaskPopper, BWaitingBox, ConfirmDialog, CellActionButton },
   data: function () {
     return {
       selected: [],
-      focused: null,
       filter: '',
       authorizations: [],
       resourcesTypes: this.$root.config.admin.resourcesTypes,
@@ -273,11 +268,6 @@ export default {
           this.loading = false
         })
     }),
-    getClasses: function(authorization) {
-      const classes = []
-      if (authorization !== this.focused) classes.push('invisible')
-      return classes
-    },
     prepareEdit: function (authorization) {
       // In case we press edit and we are in the middle to add a new authorization, that extra "unfinished"
       // auth needs to be removed from list.
