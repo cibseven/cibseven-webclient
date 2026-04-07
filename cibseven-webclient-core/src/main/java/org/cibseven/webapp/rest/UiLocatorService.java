fd3aec869cd20861e6efb8580212c8b2fbea6220
Marius Schatz <mariussc@cib.de>
2026-04-07 11:41:02 +0200

    updating pom.xml for new maven version

--- cibseven-webclient-core/src/main/java/org/cibseven/webapp/rest/UiLocatorService.java
+++ cibseven-webclient-core/src/main/java/org/cibseven/webapp/rest/UiLocatorService.java
@@ -1,17 +1,24 @@
 package org.cibseven.webapp.rest;
 
+import org.cibseven.templates.uilocator.repository.ExternalUiTaskService;
+import org.cibseven.templates.uilocator.repository.MemoryExternalUiTaskProvider;
+import org.cibseven.templates.uilocator.repository.SevenEngineRestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.cibseven.templates.uilocator.repository.ExternalUiTaskProvider;
-import org.cibseven.templates.uilocator.repository.ExternalUiTaskService;
-import org.cibseven.templates.uilocator.repository.MemoryExternalUiTaskProvider;
-import lombok.Getter;
-
 @RestController @RequestMapping("${cibseven.webclient.services.basePath:/services/v1}/locator")
 public class UiLocatorService extends ExternalUiTaskService {
-
+	    public UiLocatorService(
+        @Value("${cibseven.webclient.engineRest.url}") String cibsevenUrl, 
+		@Value("${cibseven.webclient.engineRest.path:/engine-rest}") String engineRestPath) {
+        	super(
+            new SevenEngineRestClient(cibsevenUrl + engineRestPath),
+            new MemoryExternalUiTaskProvider()
+        );
+	}
+	
+		/* 
 	@Value("${cibseven.webclient.engineRest.url:./}") protected String cibsevenUrl;
 	
 	@Getter
@@ -19,5 +26,6 @@ public class UiLocatorService extends ExternalUiTaskService {
 	
 	public String getEngineRestUrl() {
 		return cibsevenUrl + "engine-rest";
-	}
+		}*/
+
 }
