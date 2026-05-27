9dd202d47a11824d32af45f3f886acc3a0c5181e
Serge Krot <serge.krot@cib.de>
2026-05-27 10:10:06 +0200

    Update cibseven-webclient-core/src/main/java/org/cibseven/webapp/providers/EngineProvider.java
    Co-authored-by: Patrick Fincke <patrick.fincke@cib.de>

--- cibseven-webclient-core/src/main/java/org/cibseven/webapp/providers/EngineProvider.java
+++ cibseven-webclient-core/src/main/java/org/cibseven/webapp/providers/EngineProvider.java
@@ -186,7 +186,7 @@ public class EngineProvider extends SevenProviderBase implements IEngineProvider
 			if (e.getCause() instanceof HttpClientErrorException.NotFound ||
 				// for CIB seven before 2.2.0, with auth enabled, the endpoint returns 401 instead of 404 when not found
 				e.getCause() instanceof HttpClientErrorException.Unauthorized) {
-				log.warn("Engine configuration endpoint not found at {}, falling back to legacy configuration", url);
+				log.warn("Engine configuration endpoint not found or secured at {}, falling back to legacy configuration", url);
 				return null;
 			}
 			throw e;
