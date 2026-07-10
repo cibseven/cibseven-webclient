6bd326b90791771d52dca260542c8b26ced5983e
Oliver Specht <oliver.specht@cib.de>
2026-07-10 15:47:33 +0200

    refactor(metrics) reduce code duplication in getMetrics()

--- cibseven-webclient-core/src/main/java/org/cibseven/webapp/providers/SystemProvider.java
+++ cibseven-webclient-core/src/main/java/org/cibseven/webapp/providers/SystemProvider.java
@@ -40,7 +40,8 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class SystemProvider extends SevenProviderBase implements ISystemProvider {
 
-	private int getSum(String metric, Map<String, Object> queryParams, CIBUser user) {
+	@Override
+	public int getSum(String metric, Map<String, Object> queryParams, CIBUser user) {
 		String url = getEngineRestUrl(user) + "/metrics/" + metric + "/sum";
 		String params = "";
 		for (Map.Entry<String, Object> entry : queryParams.entrySet()) {
@@ -55,13 +56,6 @@ public class SystemProvider extends SevenProviderBase implements ISystemProvider
 		return body.get("result").asInt();
 	}
 	
-	private Map<String, Object> createSumParamsMap(String metric, String startDate, String endDate) {
-	    Map<String, Object> params = new HashMap<>();
-	    params.put("metric", metric);
-	    params.put("startDate", startDate);
-	    params.put("endDate", endDate);
-	    return params;
-	}
 
 	@Override
 	public JsonNode getTelemetryData(CIBUser user) {
@@ -83,82 +77,4 @@ public class SystemProvider extends SevenProviderBase implements ISystemProvider
 		}
 	}
 
-	protected Collection<Metric> getAnnualMetrics(Map<String, Object> queryParams, CIBUser user) {
-		Collection<Metric> metrics = new ArrayList<>();
-		int currentYear = ZonedDateTime.now(ZoneId.systemDefault()).getYear();
-		for (int year = currentYear; year > 2012; year--) {
-			Collection<Metric> yearMetrics = getAnnualMetricsForYear(queryParams, user, year);
-			metrics.addAll(yearMetrics);
-			// if all metrics for the year are zero, we can stop fetching
-			boolean allZero = yearMetrics.stream().allMatch(m -> m.getSum() == 0);
-			if (allZero) {
-				break;
-			}
-		}
-		return metrics;
-	}
-
-	protected Collection<Metric> getAnnualMetricsForYear(Map<String, Object> queryParams, CIBUser user, int year) {
-		Collection<Metric> metrics = new ArrayList<>();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
-
-		List<String> metricNames = Optional.ofNullable(queryParams.get("metrics"))
-			.map(Object::toString)
-			.filter(s -> !s.isEmpty())
-			.map(s -> Arrays.asList(s.split(",")))
-			.orElse(Arrays.asList("process-instances", "decision-instances", "task-users"));
-
-		for (String metric : metricNames) {
-
-			String startOfYear = ZonedDateTime.now(ZoneId.systemDefault()).withDayOfYear(1).withYear(year).format(formatter);
-			String endOfYear = ZonedDateTime.now(ZoneId.systemDefault()).withDayOfYear(1).withYear(year + 1).format(formatter);
-			Map<String, Object> params = createSumParamsMap(metric, startOfYear, endOfYear);
-
-			int count = getSum(metric, params, user);
-
-			Metric metricsData = new Metric();
-			metricsData.setMetric(metric);
-			metricsData.setSubscriptionYear(year);
-			metricsData.setSum(count);
-
-			metrics.add(metricsData);
-		}
-
-		return metrics;
-	}
-
-	protected Collection<Metric> getMonthlyMetrics(Map<String, Object> queryParams, CIBUser user) {
-		Collection<Metric> metrics = new ArrayList<>();
-		List<String> metricNames = Optional.ofNullable(queryParams.get("metrics"))
-			.map(Object::toString)
-			.filter(s -> !s.isEmpty())
-			.map(s -> Arrays.asList(s.split(",")))
-			.orElse(Arrays.asList("process-instances", "decision-instances", "task-users"));
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
-		String subsStartDate = queryParams.get("subscriptionStartDate").toString();
-		ZonedDateTime subsStartDateParsed = ZonedDateTime.parse(subsStartDate, formatter);
-
-		String startDate = queryParams.get("startDate").toString();
-		ZonedDateTime startDateParsed = ZonedDateTime.parse(startDate, formatter);
-
-		for (ZonedDateTime stDate = startDateParsed; !stDate.isAfter(subsStartDateParsed); stDate = stDate.plusMonths(1)) {
-			ZonedDateTime startDayM = stDate.with(TemporalAdjusters.firstDayOfMonth()).withHour(0).withMinute(0).withSecond(0).withNano(0);
-			ZonedDateTime endDayM = stDate.with(TemporalAdjusters.lastDayOfMonth()).withHour(23).withMinute(59).withSecond(59).withNano(999_000_000);
-			for (String metric : metricNames) {
-
-				Map<String, Object> params = createSumParamsMap(metric, startDayM.format(formatter), endDayM.format(formatter));
-				int count = getSum(metric, params, user);
-
-				Metric metricsData = new Metric();
-				metricsData.setMetric(metric);
-				metricsData.setSubscriptionYear(startDayM.getYear());
-				metricsData.setSubscriptionMonth(startDayM.getMonthValue());
-				metricsData.setSum(count);
-
-				metrics.add(metricsData);
-			}
-		}
-
-		return metrics;
-	}
 }
