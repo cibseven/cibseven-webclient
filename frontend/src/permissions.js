22c08d201f62430fb996dc8f50051b192942cbda
Serge Krot <serge.krot@cib.de>
2026-07-30 11:37:33 +0200

    added helper methods

--- frontend/src/permissions.js
+++ frontend/src/permissions.js
@@ -55,8 +55,17 @@ const permissionsMixin = {
 			const permissionsCheck = this.$_permissionsMixin_setAllPermissionsObject(permissionsRequired)
 			return this.$_permissionsMixin_checkPermissionsAllowed(process, 'key', permissionsCheck)
 		},
-		canSuspendProcessInstance: function(instanceId) {
-			const requiredPermissions = { 'processInstance': ['SUSPEND'] }
+		/**
+		 * @param {string} instanceId 
+		 * @returns {boolean} `true` if the user has permission to stop the given instanceId ('ACTIVE', 'SUSPENDED' -> 'EXTERNALLY_TERMINATED'), `false` otherwise
+		 */
+		canStopProcessInstance: function(instanceId) {
+			const requiredPermissions = { 'processDefinition': ['SUSPEND'] }
+			const permissionsCheck = this.$_permissionsMixin_setAllPermissionsObject(requiredPermissions)
+			return this.$_permissionsMixin_checkPermissionsAllowed({id: instanceId}, 'id', permissionsCheck)
+		},
+		canDeleteProcessInstance: function(instanceId) {
+			const requiredPermissions = { 'processDefinition': ['DELETE_HISTORY'] }
 			const permissionsCheck = this.$_permissionsMixin_setAllPermissionsObject(requiredPermissions)
 			return this.$_permissionsMixin_checkPermissionsAllowed({id: instanceId}, 'id', permissionsCheck)
 		},
